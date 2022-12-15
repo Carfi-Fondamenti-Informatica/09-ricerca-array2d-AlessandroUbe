13e15fb922a64ce1cb407ea9eeda6f270582ddb3
AlessandroUbe <115072327+AlessandroUbe@users.noreply.github.com>
2022-12-15 11:48:29 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,8 @@
+//
+// Created by Alessandro Ubertalli on 15/12/22.
+//
 
+#ifndef ARRAY_2D_LIB_H
+#define ARRAY_2D_LIB_H
+bool find(char lista_nomi[10][20], char target_nome[20], int &posizione);
+#endif //ARRAY_2D_LIB_H
