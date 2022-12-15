3affcbe1a3980986e362162d5084d55eff6be798
AlessandroUbe <115072327+AlessandroUbe@users.noreply.github.com>
2022-12-15 11:49:44 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -9,10 +9,10 @@ bool find(char lista_nomi[10][20], char target_nome[20], int &posizione){
         for (int letter = 0; letter < 20; letter++)
         {
             if (lista_nomi [nome][letter] != target_nome[letter]){
-                letter = 20;  //20 variabile che vanno da (0-19)
+                letter = 20;  
             }
-            if (letter == 19){ //se tutte le letteri sono uguali allora va bene
-                posizione = nome;//posizione, essendo che è nell'array va da 0-19
+            if (letter == 19){ 
+                posizione = nome;
                 return true;
             }
         }
